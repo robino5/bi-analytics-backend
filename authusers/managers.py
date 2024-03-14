40dd04508c1eed3c3a20cab2fc056e467bf42da8
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-03-14 16:21:07 +0600

    :recycle: refactoaring db columns values

--- authusers/managers.py
+++ authusers/managers.py
@@ -17,13 +17,13 @@ class BaseUserManager(UserManager):
     def create_user(self, username, password, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        extra_fields.setdefault("role", "regional_manager")
+        extra_fields.setdefault("role", "REGIONAL_MANAGER")
         return self._create_user(username, password, **extra_fields)
 
     def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("role", "admin")
+        extra_fields.setdefault("role", "ADMIN")
 
         return self._create_user(username, password, **extra_fields)
 
