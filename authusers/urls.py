dd1f3f33dd7ee8368b4de5f45c37fd7d4e5c4b1e
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-04-02 16:27:42 +0600

    :recycle: refactoring routes and models

--- authusers/urls.py
+++ authusers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import ProfileViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="users")
+router.register("profiles", ProfileViewSet, basename="profiles")
 
 urlpatterns = [path("", include(router.urls))]
