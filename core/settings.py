e9c13f08dc8a9968151860dbb57d862277b6fe48
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-04-11 21:50:18 +0600

    :bookmark: deployed to server

--- core/settings.py
+++ core/settings.py
@@ -33,8 +33,21 @@ SECRET_KEY = config("SECRET_KEY", default="putyourownkey")
 
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "192.168.10.7", "127.0.0.1", "lbsl.idash.com"]
+
 CORS_ALLOW_ALL_ORIGINS = DEBUG
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",
+    "http://localhost:8080",
+    "http://127.0.0.1:8081",
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:3000",
+    "http://192.168.10.7:3000",
+    "http://192.168.10.7",
+    "http://192.168.10.7:8080",
+    "http://lbsl.idash.com:3000",
+]
 
 
 INSTALLED_APPS = [
