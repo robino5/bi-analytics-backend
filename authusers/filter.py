3979be85fdabe3d27e9420a0f8de37ea515544d0
jiaulislam <jiaulislam.ict.bd@gmail.com>
2024-10-28 23:42:13 +0600

    refactor(branch_filter): refactoring branch id to branch name filter

--- authusers/filter.py
+++ authusers/filter.py
@@ -6,9 +6,9 @@ from .models import User
 
 
 class UserFilter(df.FilterSet):
-    branch_id = df.BaseInFilter(field_name="profile__branch_id", lookup_expr="in")
+    branch = df.BaseInFilter(field_name="profile__branch_name", lookup_expr="in")
     role = df.BaseInFilter(field_name="role", lookup_expr="in")
-    logged_in_today = df.BooleanFilter(
+    signedInToday = df.BooleanFilter(
         method="filter_logged_in_today", label="Is Logged in Today ?"
     )
 
@@ -18,4 +18,4 @@ class UserFilter(df.FilterSet):
 
     class Meta:
         model = User
-        fields = ("username", "email", "is_active", "role", "branch_id")
+        fields = ("username", "email", "is_active", "role", "branch")
